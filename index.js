da29e3e582fc8f2dd67d68d78465f90ceb48fc56
joe llorens <joellorens@gmail.com>
2023-07-30 16:13:52 -0400

    arrayslab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+// const firstCat = Milo
+// const secondCat = Otis
+// const thirdCat = Garfield
+
+// function logCats("Milo", "Otis", "Garfield");
+// console.log(Cats);
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name) {
+    cats.push(name)
+    return cats
+
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+    return cats
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+    return cats
+}
+function appendCat(name) {
+    const backCats = [...cats, "Broom"]
+    return backCats
+}
+function prependCat(name) {
+    const frontCats = ["Arnold", ...cats]
+    return frontCats
+}
+function removeLastCat(name) {
+    const lessCats = cats.slice(0, -1)
+    return lessCats
+}
+function removeFirstCat(name) {
+    const fewerCats = cats.slice(1)
+    return fewerCats
+}
